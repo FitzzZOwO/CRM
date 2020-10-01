d2aa5e44ef891eadb2782840e4f6531b86382c04
Fitz <2259975491@qq.com>
2020-10-01 22:23:09 +0800

    test

--- src/test/java/com/wenjing/test/test01.java
+++ src/test/java/com/wenjing/test/test01.java
@@ -2,7 +2,8 @@ package com.wenjing.test;
 
 public class test01 {
     public static void main(String[] args) {
-        System.out.println(System.getProperty("file.encoding"));
+        System.out.println("this is testClass");
+        //System.out.println(System.getProperty("file.encoding"));
         //5555
     }
 }
