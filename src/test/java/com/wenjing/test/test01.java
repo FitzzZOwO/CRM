14ff1961bca79150395a84b96eb75f9803c7d5b2
FitzzZOwO <65837747+FitzzZOwO@users.noreply.github.com>
2020-10-01 22:24:17 +0800

    Update test01.java

--- src/test/java/com/wenjing/test/test01.java
+++ src/test/java/com/wenjing/test/test01.java
@@ -4,6 +4,6 @@ public class test01 {
     public static void main(String[] args) {
         System.out.println("this is testClass");
         //System.out.println(System.getProperty("file.encoding"));
-        //5555
+        //5555666
     }
 }
